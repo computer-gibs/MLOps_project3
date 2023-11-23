6e5cb45a49736b18d81033cfb38f7f13e511affd
AlexanderK <119937312+computer-gibs@users.noreply.github.com>
2023-11-24 01:13:30 +0500

    training_model.py: one-hot encoder addition

--- scripts/training_model.py
+++ scripts/training_model.py
@@ -1,28 +1,36 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 import joblib
 
 def train_model(X_train_path, y_train_path, model_save_path):
     X_train = pd.read_csv(X_train_path)
     y_train = pd.read_csv(y_train_path)
-
-    # преобразование категориальных переменных в числовые
-    label_encoder = LabelEncoder()
-    X_train['country_code'] = label_encoder.fit_transform(X_train['country_code'])
-
-    # создание и обучение модели
-    model = RandomForestRegressor(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train.values.ravel())
     
-    joblib.dump(model, model_save_path)
-    joblib.dump(label_encoder, '../models/label_encoder.pkl')
+    # категориальные признаки находятся в первых двух столбцах
+    categorical_features = ['country_code', 'country_name']
+    # cоздаем трансформер для категориальных признаков
+    categorical_transformer = OneHotEncoder(handle_unknown='ignore')
     
-    return model
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('cat', categorical_transformer, categorical_features)],
+        remainder='passthrough')
+    
+    pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                               ('model', RandomForestRegressor(n_estimators=100, random_state=42))])
+    
+    pipeline.fit(X_train, y_train.values.ravel())
+    
+    joblib.dump(pipeline, model_save_path)
+    
+    return pipeline
 
 X_train_path = '../data/X_train.csv'
 y_train_path = '../data/y_train.csv'
 
 model_save_path = '../models/co2_emission_predictor_model.pkl'
+
 trained_model = train_model(X_train_path, y_train_path, model_save_path)
