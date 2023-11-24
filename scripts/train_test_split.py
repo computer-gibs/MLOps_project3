830f77ecc0b0a480e44ce88afa2da10e54aad15a
AlexanderK <alexandr.nikitin2404@gmail.com>
2023-11-24 22:47:21 +0500

    train test split fixed again

--- scripts/train_test_split.py
+++ scripts/train_test_split.py
@@ -1,40 +1,22 @@
 import pandas as pd
+import os
 
-def load_and_split_data(data_path):
-    data = pd.read_csv(data_path)
-    data['year'] = pd.to_datetime(data['year'])
-    data = data.sort_values(by=['country_code', 'year'])
-    
-    # создаем словарь для хранения обучающих и тестовых данных по каждой стране
-    train_data = {}
-    test_data = {}
-    
-    # проходим по каждой стране и разделяем данные на обучающую и тестовую выборки
-    for country in data['country_code'].unique():
-        country_data = data[data['country_code'] == country]
-        
-        split_point = int(len(country_data) * 0.8)
-        
-        #добавляем данные в словари
-        train_data[country] = country_data[:split_point]
-        test_data[country] = country_data[split_point:]
-    
-    # конвертируем словари обратно в DataFrame
-    train_df = pd.concat(train_data.values(), ignore_index=True)
-    test_df = pd.concat(test_data.values(), ignore_index=True)
-    
-    X_train = train_df.drop('value', axis=1)
-    y_train = train_df['value']
-    X_test = test_df.drop('value', axis=1)
-    y_test = test_df['value']
-    
-    return X_train, X_test, y_train, y_test
+current_dir = os.path.dirname(__file__)
+data_dir = os.path.join(current_dir, '..', 'data')
 
-data_path = '../data/prepared_co2_emissions.csv'
+file_path = os.path.join(data_dir, 'co2_emissions_mt_by_country.csv')
+df = pd.read_csv(file_path)
 
-X_train, X_test, y_train, y_test = load_and_split_data(data_path)
+# зададим границу разделения на обучающую и тестовую выборки
+split_year = 2010
 
-X_train.to_csv('../data/X_train.csv', index=False)
-y_train.to_csv('../data/y_train.csv', index=False)
-X_test.to_csv('../data/X_test.csv', index=False)
-y_test.to_csv('../data/y_test.csv', index=False)
+# разделение данных на обучающий и тестовый наборы
+train = df[df['year'] <= split_year]
+test = df[df['year'] > split_year]
+
+# сохранение обучающего и тестового наборов данных
+train_file_path = os.path.join(data_dir, 'train_set.csv')
+test_file_path = os.path.join(data_dir, 'test_set.csv')
+
+train.to_csv(train_file_path, index=False)
+test.to_csv(test_file_path, index=False)
