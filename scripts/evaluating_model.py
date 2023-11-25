7e93fc0f48628116e0f034e0b1cccb3d53ac1b87
AlexanderK <alexandr.nikitin2404@gmail.com>
2023-11-25 20:05:56 +0500

    mlflow integration

--- scripts/evaluating_model.py
+++ scripts/evaluating_model.py
@@ -3,6 +3,10 @@ import os
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import joblib
+import mlflow
+from mlflow import log_metric
+
+mlflow.start_run()
 
 current_dir = os.path.dirname(__file__)
 data_dir = os.path.join(current_dir, '..', 'data')
@@ -22,6 +26,9 @@ y_pred = model.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 
+log_metric("mae", mae)
+log_metric("mse", mse)
+
 print(f"Средняя абсолютная ошибка (MAE): {mae:.2f}")
 print(f"Среднеквадратическая ошибка (MSE): {mse:.2f}")
 
@@ -29,3 +36,7 @@ results_file_path = os.path.join(models_dir, 'model_evaluation_results.txt')
 with open(results_file_path, 'w') as file:
     file.write(f"Средняя абсолютная ошибка (MAE): {mae:.2f}\n")
     file.write(f"Среднеквадратическая ошибка (MSE): {mse:.2f}\n")
+
+log_artifacts(models_dir)
+
+mlflow.end_run()
--- scripts/training_model.py
+++ scripts/training_model.py
@@ -3,6 +3,10 @@ import os
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import joblib
+import mlflow
+from mlflow import log_metric, log_param, log_artifacts
+
+mlflow.start_run()
 
 current_dir = os.path.dirname(__file__)
 data_dir = os.path.join(current_dir, '..', 'data')
@@ -16,7 +20,15 @@ y = train_data['value']
 
 # создание и обучение модели линейной регрессии
 model = LinearRegression()
+
+log_param("model_type", "linear_regression")
+log_param("features", X.columns.tolist())
+
 model.fit(X, y)
+
+log_metric("training_score", model.score(X, y))
 
 model_file_path = os.path.join(models_dir, 'co2_emissions_model.joblib')
 joblib.dump(model, model_file_path)
+mlflow.sklearn.log_model(model, "model")
+mlflow.end_run()
